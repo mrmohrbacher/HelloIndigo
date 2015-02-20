3fe7d53947f9cfdf089e6066e6c8399fccce1f60
Mike Mohrbacher <mike@blackriverinc.com>
2015-02-19 21:27:57 -0600

    Dump CSV.

--- LibraryCheckout.WebAppMVC/Controllers/LibraryController.cs
+++ LibraryCheckout.WebAppMVC/Controllers/LibraryController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,6 +19,55 @@ namespace LibraryCheckout.WebAppMVC.Controllers
 	{
 	public class LibraryController : Controller
 		{
+		[HttpGet]
+		public ActionResult Export()
+			{
+			Book[] books = null;
+			try
+				{
+				IDataStoreProvider provider = WebConfigProvider.Open();
+				if (provider == null)
+					{
+					throw new ApplicationException("Could not open Web Configuration Settings.");
+					}
+				provider = new CloudSettingsProvider(provider);
+				IKeyedDataStore settings = new KeyedDataStore(provider);
+
+				string endpointName = settings["LibraryServiceEndpoint"] as string;
+				if (endpointName == null)
+					{
+					throw new ApplicationException("Could not find 'LibraryServiceEndpoint' in configuration settings.");
+					}
+				Debug.WriteLine(string.Format("LibraryServiceEndpoint='{0}'", endpointName));
+
+				// ---------------------------------------------------------
+				// 
+				// ---------------------------------------------------------
+				using (LibraryServiceClient proxy = new LibraryServiceClient(endpointName))
+					{
+					proxy.List(null, out books);
+					}
+
+				}
+			catch (Exception exp)
+				{
+				Request.PostError(exp, false);
+				}
+
+			Response.ContentType = "text/csv";
+			Response.ContentEncoding = System.Text.ASCIIEncoding.ASCII;
+			StringBuilder content = new StringBuilder();
+			StringWriter writer = new StringWriter(content);
+			foreach (var book in books)
+				{
+				if (content.Length == 0)
+					writer.WriteLine(book.ToPropertyNameList("ISBN Author Title Publisher Synopsis"));
+				writer.WriteLine(book.ToCSVString("ISBN Author Title Publisher Synopsis"));
+				}
+
+			return File(new System.Text.UTF8Encoding().GetBytes(content.ToString()), "text/csv", "Library.csv");
+			}
+
 		//
 		// GET: /Library/
 		[HttpGet]
